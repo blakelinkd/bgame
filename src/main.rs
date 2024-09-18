7de7396adbf48c87abd3dd22d16d1aac0429388f
Blake Link <blakelinkd@outlook.com>
2024-09-18 16:01:37 -0500

    Update .gitignore and Cargo.toml, add map.gltf and modify main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,45 @@
-//! This example demonstrates how to use the `Camera::viewport_to_world` method.
-
 use std::f32::consts::PI;
 
 use bevy::prelude::*;
-use bevy::input::mouse::MouseButton;
+use avian3d::prelude::*;
+use bevy::input::mouse::{MouseButton, MouseMotion};
 use bevy::render::{
     render_asset::RenderAssetUsages,
     render_resource::{Extent3d, TextureDimension, TextureFormat},
 };
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .add_systems(Update, draw_cursor)
-        .add_systems(Update, update_projectiles)
-        .run();
+use bevy::input::ButtonInput;
+use bevy::prelude::Res;
+use bevy::window::CursorGrabMode;
+
+#[derive(Debug, Component)]
+struct Player;
+
+#[derive(Debug, Component)]
+struct WorldModelCamera;
+
+#[derive(Component)]
+struct Health {
+    is_alive: bool,
 }
 
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default(), PhysicsDebugPlugin::default()))
+        .insert_gizmo_config(
+            PhysicsGizmos {
+                aabb_color: Some(Color::WHITE),
+                ..default()
+            },
+            GizmoConfig::default(),
+        )
+        .insert_resource(Gravity(Vec3::NEG_Y * 19.6))
+        .add_systems(Startup, setup)
+        .add_systems(Update, fire_weapon)
+        .add_systems(Update, update_projectiles)
+        .add_systems(Update, move_player)
+        .add_systems(Startup, lock_and_hide_cursor)
+        .run();
+}
 
 fn update_projectiles(
     mut projectiles: Query<(&mut Transform, &Projectile)>,
@@ -27,14 +50,20 @@ fn update_projectiles(
     }
 }
 
+fn lock_and_hide_cursor(mut windows: Query<&mut Window>) {
+    let mut window = windows.single_mut();
+    window.cursor.visible = false;
+    window.cursor.grab_mode = CursorGrabMode::Locked;
+}
 
-fn draw_cursor(
+fn fire_weapon(
     mut commands: Commands,
     camera_query: Query<(&Camera, &GlobalTransform)>,
     windows: Query<&Window>,
     mouse_buttons: Res<ButtonInput<MouseButton>>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     let (camera, camera_transform) = camera_query.single();
 
@@ -55,21 +84,33 @@ fn draw_cursor(
 
         println!("hit at {:?}", point);
 
-        // Spawn a projectile and set its velocity towards the hit point.
-        commands.spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(Sphere::default().mesh().ico(5).unwrap())),
-            material: materials.add(StandardMaterial { base_color: Color::WHITE, ..default() }),
-            transform: Transform::from_translation(camera_transform.translation()), // Start from the camera position
-            ..Default::default()
-        })
-        .insert(Projectile {
-            velocity: (point - camera_transform.translation()).normalize() * 10.0, // adjust the speed as needed
-            ..default()
-        });
+        // Instead of creating a trimesh collider, use a simpler shape
+        let collider = Collider::sphere(0.5); // Replace the size with an appropriate value
+        commands.spawn((
+            RigidBody::Dynamic,
+            collider,
+            DebugRender::default().with_collider_color(Color::srgb(0.0, 1.0, 0.0)),
+            PbrBundle {
+                mesh: meshes.add(Mesh::from(Sphere::default().mesh().ico(5).unwrap())), // Use the existing mesh
+                material: materials.add(StandardMaterial { base_color: Color::WHITE, ..default() }),
+                transform: Transform::from_translation(camera_transform.translation()), // Start from the camera position
+                ..Default::default()
+            },
+            Projectile {
+                velocity: (point - camera_transform.translation()).normalize() * 40.0, // Adjust the speed as needed
+                ..default()
+            }
+        ));
+
+        commands.spawn((
+            AudioBundle {
+                source: asset_server.load("sounds/queef.ogg"),
+                ..default()
+            },
+            MyMusic,
+        ));
     }
 }
-
-
 
 #[derive(Component)]
 struct Ground;
@@ -90,16 +131,61 @@ impl Default for Projectile {
     }
 }
 
-
 const SHAPES_X_EXTENT: f32 = 14.0;
 const Z_EXTENT: f32 = 5.0;
 
+fn move_player(
+    mut mouse_motion: EventReader<MouseMotion>,
+    mut player: Query<(&mut Transform, &mut ExternalImpulse, &mut Health), With<Player>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+) {
+    let (mut transform, mut external_impulse, health) = player.single_mut();
+    for motion in mouse_motion.read() {
+        let yaw = -motion.delta.x * 0.003;
+        let pitch = -motion.delta.y * 0.002;
+        // Order of rotations is important, see <https://gamedev.stackexchange.com/a/136175/103059>
+        transform.rotate_y(yaw);
+        transform.rotate_local_x(pitch);
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        let forward = transform.forward();
+        transform.translation += forward * 0.1; // Adjust the speed as needed
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        let backward = -transform.forward();
+        transform.translation += backward * 0.1; // Adjust the speed as needed
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        let left = -transform.right();
+        transform.translation += left * 0.1; // Adjust the speed as needed
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        let right = transform.right();
+        transform.translation += right * 0.1; // Adjust the speed as needed
+    }
+
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        std::process::exit(0);
+    }
+
+
+
+    if keyboard_input.just_pressed(KeyCode::Space) && health.is_alive {
+        // Apply an upward force to simulate a jump
+        external_impulse.apply_impulse(Vec3::Y * 80.0);
+    }
+}
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut images: ResMut<Assets<Image>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
 ) {
     let debug_material = materials.add(StandardMaterial {
         base_color_texture: Some(images.add(uv_debug_texture())),
@@ -113,36 +199,94 @@ fn setup(
         meshes.add(Cylinder::default()),
         meshes.add(Cone::default()),
         meshes.add(ConicalFrustum::default()),
-        meshes.add(Sphere::default().mesh().ico(5).unwrap()),
-        meshes.add(Sphere::default().mesh().uv(32, 18)),
+        // meshes.add(Sphere::default().mesh().ico(5).unwrap()),
+        // meshes.add(Sphere::default().mesh().uv(32, 18)),
     ];
 
     let num_shapes = shapes.len();
 
-    for (i, shape) in shapes.into_iter().enumerate() {
-        commands.spawn((
-            PbrBundle {
-                mesh: shape,
-                material: debug_material.clone(),
-                transform: Transform::from_xyz(
-                    -SHAPES_X_EXTENT / 2. + i as f32 / (num_shapes - 1) as f32 * SHAPES_X_EXTENT,
-                    2.0,
-                    Z_EXTENT / 2.,
-                )
-                .with_rotation(Quat::from_rotation_x(-PI / 4.)),
+    commands.spawn((
+        AudioBundle {
+            source: asset_server.load("sounds/lazer.ogg"),
+            ..default()
+        },
+        MyMusic,
+    ));
+
+    commands.spawn((
+        Player,
+        SpatialBundle {
+            transform: Transform::from_xyz(0.0, 1.0, 0.0),
+            ..default()
+        },
+        RigidBody::Dynamic,
+        Collider::capsule(1.0, 0.5), // Add a collision capsule to the player
+        ExternalImpulse::default(),
+        Health { is_alive: true }, // Initialize the player as alive
+        LockedAxes::ROTATION_LOCKED, // Lock the rotation axes
+        DebugRender::default().with_collider_color(Color::srgb(0.0, 1.0, 0.0)),
+    )).with_children(|parent| {
+        parent.spawn((
+            WorldModelCamera,
+            Camera3dBundle {
+                projection: PerspectiveProjection {
+                    fov: 90.0_f32.to_radians(),
+                    ..default()
+                }
+                .into(),
                 ..default()
             },
+        ));
+    });
+
+    for (i, shape) in shapes.into_iter().enumerate() {
+        let collider = match shape {
+            _ if shape == meshes.add(Cuboid::default()) => Collider::cuboid(0.5, 0.5, 0.5),
+            _ if shape == meshes.add(Capsule3d::default()) => Collider::capsule(1.0, 0.5),
+            _ if shape == meshes.add(Torus::default()) => Collider::capsule(1.0, 0.3),
+            _ if shape == meshes.add(Cylinder::default()) => Collider::cylinder(1.0, 0.5),
+            _ if shape == meshes.add(Cone::default()) => Collider::cone(1.0, 0.5),
+            _ => Collider::capsule(1.0, 0.5), // Default case
+        };
+
+        commands.spawn((
+            PbrBundle {
+            mesh: shape,
+            material: debug_material.clone(),
+            transform: Transform::from_xyz(
+                -SHAPES_X_EXTENT + i as f32 * (SHAPES_X_EXTENT / (num_shapes - 1) as f32 * 2.0),
+                2.0,
+                Z_EXTENT / 2.,
+            )
+            .with_rotation(Quat::from_rotation_x(-PI / 4.)),
+            ..default()
+            },
             Shape,
+            RigidBody::Dynamic,
+            collider,
+            DebugRender::default().with_collider_color(Color::srgb(1.0, 0.0, 0.0)),
         ));
     }
+
+    
+    
+    
     // plane
+    let plane_mesh_builder = Plane3d::default().mesh().size(2000., 2000.);
+    let plane_mesh = Mesh::from(plane_mesh_builder);
+    let plane_collider = Collider::trimesh_from_mesh(&plane_mesh).unwrap();
     commands.spawn((
+        plane_collider,
         PbrBundle {
-            mesh: meshes.add(Plane3d::default().mesh().size(20., 20.)),
-            material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
+            mesh: meshes.add(plane_mesh),
+            material: materials.add(StandardMaterial {
+                base_color: Color::srgb(0.8, 0.8, 0.8),
+                ..default()
+            }),
             ..default()
         },
         Ground,
+        RigidBody::Kinematic,
     ));
 
     // light
@@ -150,13 +294,10 @@ fn setup(
         transform: Transform::from_translation(Vec3::ONE).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-
-    // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(15.0, 5.0, 15.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
 }
+
+#[derive(Component)]
+struct MyMusic;
 
 /// Creates a colorful test pattern
 fn uv_debug_texture() -> Image {
